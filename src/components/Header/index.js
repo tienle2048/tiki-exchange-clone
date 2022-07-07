83a936e848238ee78c62fc8501ce8257e45ebe82
tienle2048 <leviettien456789@gmail.com>
2022-07-07 23:27:34 +0700

    chi tiet phan header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,50 @@
+import classNames from 'classnames/bind'
+import styles from './Header.module.scss'
+
+import logotiki from '../../assets/images/logotiki.png'
+import astra from '../../assets/images/astra.png'
+
+
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import {faGear} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+const cx = classNames.bind(styles)
 function Header() {
-    return ( 
-        <div>
-            Header
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('left')}>
+                <div className={cx('logo')}>
+                    <img src={logotiki} alt='logo'></img>
+                </div>
+                <div className={cx('rewards')}>
+                    <a href="https://tiki.vn/sep" target="_blank" rel="noreferrer">Tiki Rewards</a>
+                    <a href="https://tiki.vn/sep" target="_blank" rel="noreferrer">Góp ý</a>
+                </div>
+            </div>
+            <div className={cx('rigth')}>
+                <div className={cx('label')}>Tài sản của tôi:</div>
+                <div className={cx('astra')}>
+                    <img src={astra} alt='logo'></img>
+                    <div>1000</div>
+                </div>
+                <div className={cx('okla')}></div>
+                <div className={cx('tikixu')}>
+                    <img src={astra} alt='logo'></img>
+                    <div>1000</div>
+                </div>
+                <div className={cx('okla')}></div>
+                <div className={cx('icon')}>
+                    <FontAwesomeIcon className={cx('user')} icon={faUser} />
+                </div>
+                <div className={cx('icon')}>
+                    <FontAwesomeIcon className={cx('setting')} icon={faGear} />
+                </div>
+
+            </div>
         </div>
-     );
+    );
 }
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './components/GlobalStyles'
 
 ReactDOM.render(
   <React.StrictMode>
+  <GlobalStyles>
     <App />
+  </GlobalStyles>
   </React.StrictMode>,
   document.getElementById('root')
 );
