7ac02e0af3bc6be35724e53cfaf5ea404e4a71ef
tienle2048 <leviettien456789@gmail.com>
2022-07-20 18:48:55 +0700

    ui order form

--- src/components/OrderForm/index.js
+++ src/components/OrderForm/index.js
@@ -6,10 +6,36 @@ import tikixu from '../../assets/svg/tikixu.svg'
 
 import handle from '../../assets/svg/arrows-up-down-left-right-solid.svg'
 
+import 'antd/dist/antd.min.css'
+
+import { useState } from 'react'
+import { Slider } from 'antd'
+
+
 const cx = classNames.bind(style)
 
 
 function OrderForm() {
+
+    const [volumeBuy, setVolumeBuy] = useState('')
+    const [volumeSell, setVolumeSell] = useState('')
+
+    const changeVolumeBuy = (newValue) => {
+        if (newValue === "") setVolumeBuy(newValue)
+        else {
+            newValue = Number(newValue)
+            if (!isNaN(newValue)) setVolumeBuy(newValue)
+        }
+    }
+    const changeVolumeSell = (newValue) => {
+        if (newValue === "") setVolumeBuy(newValue)
+        else {
+            newValue = Number(newValue)
+            if (!isNaN(newValue)) setVolumeSell(newValue)
+        }
+    }
+
+
     return (
         <>
             <span className='drag-handle'>
@@ -34,12 +60,21 @@ function OrderForm() {
                         </div>
                         <div className={cx('price-buy')}>
                             <div className={cx('label')}>KL</div>
-                            <input type='text' ></input>
+                            <input type='text' value={volumeBuy} onChange={(e) => changeVolumeBuy(e.target.value)}></input>
                             <div className={cx('logo-token')}>
                                 <img src={astra} alt='logo'></img>
                             </div>
                         </div>
-                        <div className={cx('volume-slider-buy')}></div>
+                        <div className={cx('volume-slider-buy')}>
+                            <Slider
+                                style={{ margin: '10px 6px' }}
+                                min={0}
+                                max={100}
+                                marks={{ 0: <></>, 25: <></>, 50: <></>, 75: <></>, 100: <></> }}
+                                onChange={changeVolumeBuy}
+                                value={volumeBuy}
+                            />
+                        </div>
                         <div className={cx('previewAmount')}>
                             <div>Số lượng Xu cần thiết :</div>
                             <div>1000</div>
@@ -47,12 +82,12 @@ function OrderForm() {
                                 <img src={tikixu} alt='logo astra'></img>
                             </div>
                         </div>
-                        
+
                         <button className={cx('buy-button-buy')}>Mua</button>
-                        
+
                     </div>
                     <div className={cx('buy-form')}>
-                    <div className={cx('price-buy')}>
+                        <div className={cx('price-buy')}>
                             <div className={cx('label')}>Giá</div>
                             <input type='text' ></input>
                             <div className={cx('logo-token')}>
@@ -61,12 +96,21 @@ function OrderForm() {
                         </div>
                         <div className={cx('price-buy')}>
                             <div className={cx('label')}>KL</div>
-                            <input type='text' ></input>
+                            <input type='text' value={volumeSell} onChange={(e) => changeVolumeSell(e.target.value)}></input>
                             <div className={cx('logo-token')}>
                                 <img src={astra} alt='logo'></img>
                             </div>
                         </div>
-                        <div className={cx('volume-slider-buy')}></div>
+                        <div className={cx('volume-slider-buy')}>
+                            <Slider
+                                style={{ margin: '10px 6px' }}
+                                min={0}
+                                max={100}
+                                marks={{ 0: <></>, 25: <></>, 50: <></>, 75: <></>, 100: <></> }}
+                                onChange={changeVolumeSell}
+                                value={volumeSell}
+                            />
+                        </div>
                         <div className={cx('previewAmount')}>
                             <div>Số lượng Xu ước tính nhận được:</div>
                             <div>1000</div>
@@ -74,7 +118,7 @@ function OrderForm() {
                                 <img src={tikixu} alt='logo astra'></img>
                             </div>
                         </div>
-                        
+
                         <button className={cx('buy-button-sell')}>Bán</button>
                     </div>
                 </div>
--- src/components/Orders/index.js
+++ src/components/Orders/index.js
@@ -1,8 +1,18 @@
+import handle from '../../assets/svg/arrows-up-down-left-right-solid.svg'
+
+
+
+
 function Orders() {
     return ( 
+        <>
+            <span className='drag-handle'>
+                <img src={handle} alt='handle'></img>
+            </span>
         <div>
             Order
         </div>
+        </>
      );
 }
 
