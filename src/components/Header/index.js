55daeb7d8c082fc712aa8e2487593985af018096
tienle2048 <leviettien456789@gmail.com>
2022-07-25 23:01:01 +0700

    logout fix dropdown

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,19 +4,26 @@ import styles from './Header.module.scss'
 import logotiki from '../../assets/images/logotiki.png'
 import astra from '../../assets/images/astra.png'
 
-import Button from '../Globalcomponents/Button'
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGear } from '@fortawesome/free-solid-svg-icons'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
 
 import Tippy from '@tippyjs/react';
 
+import {useDispatch,useSelector} from 'react-redux'
+
+import {userActions} from "../../actions/user"
+
 
 const cx = classNames.bind(styles)
 function Header() {
 
-    let islogin = true
+    const dispatch =useDispatch()
+
+    const handleLogout = ()=>{
+        dispatch(userActions.logout())
+    }
+
     return (
         <div className={cx('wrapper')}>
             <div className={cx('left')}>
@@ -29,56 +36,46 @@ function Header() {
                 </div>
             </div>
             <div className={cx('rigth')} id="parent">
-                {islogin ?
-                    <>
-                        <div className={cx('label')}>Tài sản của tôi:</div>
-                        <div className={cx('astra')}>
-                            <img src={astra} alt='logo'></img>
-                            <div>1000</div>
+                <div className={cx('label')}>Tài sản của tôi:</div>
+                <div className={cx('astra')}>
+                    <img src={astra} alt='logo'></img>
+                    <div>1000</div>
+                </div>
+                <div className={cx('okla')}></div>
+                <div className={cx('tikixu')}>
+                    <img src={astra} alt='logo'></img>
+                    <div>1000</div>
+                </div>
+                <div className={cx('okla')}></div>
+                <Tippy
+                    placement='bottom-start'
+                    interactive
+                    content={
+                        <div tabIndex="0">
+                            <ul className={cx('dropdown')}>
+                                <li className={cx('userinfo')}>
+                                    <div className={cx('avatar')}>
+                                        <img src={astra} alt='logo'></img>
+                                    </div>
+                                    <div>
+                                        <div>tien le </div>
+                                        <div>leviettien456789@gmail.com</div>
+                                    </div>
+                                </li>
+                                <li className={cx('bar')}></li>
+                                <li className={cx('logout')} onClick={handleLogout}>Đăng xuất</li>
+                            </ul>
                         </div>
-                        <div className={cx('okla')}></div>
-                        <div className={cx('tikixu')}>
-                            <img src={astra} alt='logo'></img>
-                            <div>1000</div>
-                        </div>
-                        <div className={cx('okla')}></div>
-                        <Tippy
-                            placement='bottom-start'
-                            interactive
-                            render={attrs => (
-                                <div tabIndex="0">
-                                <ul className={cx('dropdown')}>
-                                    <li className={cx('userinfo')}>
-                                        <div className={cx('avatar')}>
-                                            <img src={astra} alt='logo'></img>
-                                        </div>
-                                        <div>
-                                            <div>tien le </div>
-                                            <div>leviettien456789@gmail.com</div>
-                                        </div>
-                                    </li>
-                                    <li className={cx('bar')}></li>
-                                    <li className={cx('logout')}>Đăng xuất</li>
-                                </ul>
-                                </div>
-                            )}
-                        >
-                            <div className={cx('icon')} aria-expanded="false">
-                                <FontAwesomeIcon className={cx('user')} icon={faUser} />
-                            </div>
-                        </Tippy>
+                    }
+                >
+                    <div className={cx('icon')} aria-expanded="false">
+                        <FontAwesomeIcon className={cx('user')} icon={faUser} />
+                    </div>
+                </Tippy>
+                <div className={cx('icon')}>
+                    <FontAwesomeIcon className={cx('setting')} icon={faGear} />
+                </div>
 
-
-
-
-                        <div className={cx('icon')}>
-                            <FontAwesomeIcon className={cx('setting')} icon={faGear} />
-                        </div>
-                    </> :
-                    <Button login onClick={() => console.log('okla')}>
-                        Đăng nhập
-                    </Button>
-                }
             </div>
         </div>
     );
